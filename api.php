ae5208b2ac7daa63b3cd479ba78b5fba2e5ce755
Ilya Denisyuk <32599637+ThattHuman@users.noreply.github.com>
2020-10-30 10:59:46 +0200

    some add

--- api.php
+++ api.php
@@ -39,6 +39,88 @@ abstract class Api
 			throw new RuntimeException('API Not Found', 404);
 		//определение действия для обработки
 		$this->action = $this->getAction();
+
+		if(method_exists($this, $this->action)) 
+			return	$this->{$this->action}();
+		else
+			throw new RuntimeException('Invalid Method', 405);
+	}
+
+	protected function response($data, $status = 500)
+	{
+		header("HTTP/1.1 " . $status . " " . $this->requestStatus($status));
+		return json_encode($data);
+	}
+
+	private function requestStatus($code)
+	{
+		$status = array(
+			200 => 'OK',
+			404 => 'Not Found',
+			405 => 'Method Not Allowed',
+			500 => 'Iternal Server Error',
+		);
+		return ($status[$code])?$status[$code]:$status[500];
+	}
+
+	protected function getAction()
+    {
+        $method = $this->method;
+        switch ($method) 
+		{
+            case 'GET':
+                if($this->requestUri){
+                    return 'viewAction';
+                } else {
+                    return 'indexAction';
+                }
+                break;
+            case 'POST':
+                return 'createAction';
+                break;
+            case 'PUT':
+                return 'updateAction';
+                break;
+            case 'DELETE':
+                return 'deleteAction';
+                break;
+            default:
+                return null;
+        }
+
+		abstract protected function indexAction();
+		abstract protected function viewAction();
+		abstract protected function createAction();
+		abstract protected function updateAction();
+		abstract protected function deleteAction();
+    }
+}
+?>
+
+<?
+require_once 'Api.php';
+require_once 'Db.php';
+require_once 'Users.php';
+
+class UsersApi extends Api
+{
+	public $apiName = 'users';
+
+	/**
+     * Метод GET
+     * Вывод списка всех записей
+     * http://ДОМЕН/users
+     * @return string
+     */
+	public function indexAction()
+	{
+		$db = (new Db())->getConnect();
+		$users = Users::getAll($db);
+		if($users)
+		{
+			return $this->response($user, 200);
+		}
+		return $this->response('Data not found', 404); 
 	}
 }
 ?>
